b52756464ea160569392155c6e964eda7e7390cb
vyou <vyou@umich.edu>
2022-04-20 07:57:01 -0400

    clean up importation errors and updated work thumbnails

--- pages/works.js
+++ pages/works.js
@@ -3,18 +3,8 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 
-import thumbInkdrop from '../public/images/works/inkdrop_eyecatch.png'
-import thumbWalknote from '../public/images/works/walknote_eyecatch.png'
-import thumbFourPainters from '../public/images/works/the-four-painters_eyecatch.jpg'
-import thumbMenkiki from '../public/images/works/menkiki_eyecatch.png'
-import thumbModeTokyo from '../public/images/works/modetokyo_eyecatch.png'
-import thumbStyly from '../public/images/works/styly_eyecatch.png'
-import thumbPichu2 from '../public/images/works/pichu2_eyecatch.png'
-import thumbFreeDBTagger from '../public/images/works/freedbtagger_eyecatch.png'
-import thumbAmembo from '../public/images/works/amembo_eyecatch.png'
-
 import thumbRNN from '../public/images/works/rnn_eyecatch.png'
-import thumbST from '../public/images/works/st_eyecatch.jpg'
+import thumbST from '../public/images/works/st_eyecatch.png'
 import thumbSocr from '../public/images/works/socr_eyecatch.png'
 import thumbEzbake from '../public/images/works/ezbake_eyecatch.png'
 
@@ -56,8 +46,8 @@ const Works = () => (
             title="SOCR"
             thumbnail={thumbSocr}
           >
-            Design and disseminate advanced methods/tools in probability, staticstics,
-            and machine learning; develop enhanced analysis and visualizations on "Big Data"
+            Design and disseminate advanced methods/tools in probability, statistics,
+            and machine learning; develop enhanced analysis and visualizations on &quot;Big Data&quot;
           </WorkGridItem>
         </Section>
 
@@ -70,6 +60,14 @@ const Works = () => (
             Implementation of a complex software that automates quality testing
             of camera lens by communicating with a thermal chamber via Modbus Protocol
           </WorkGridItem>
+        </Section>
+
+        <Section delay={0.2}>
+          <Divider my={6} />
+
+          <Heading as="h3" fontSize={20} mb={4}>
+            Older Work
+          </Heading>
         </Section>
 
       </SimpleGrid>
