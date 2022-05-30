eae56a278f44ecf613bdbedff32a08c5d48a832e
vyou <vyou@umich.edu>
2022-05-30 18:20:21 -0400

    updated source

--- components/navbar.js
+++ components/navbar.js
@@ -76,7 +76,7 @@ const Navbar = props => {
           </LinkItem>
           <LinkItem
             target="_blank"
-            href="https://github.com/craftzdog/craftzdog-homepage"
+            href="https://github.com/yyou22/personal-website"
             path={path}
             display="inline-flex"
             alignItems="center"
@@ -108,7 +108,7 @@ const Navbar = props => {
                 </NextLink>
                 <MenuItem
                   as={Link}
-                  href="https://github.com/craftzdog/craftzdog-homepage"
+                  href="https://github.com/yyou22/personal-website"
                 >
                   View Source
                 </MenuItem>
