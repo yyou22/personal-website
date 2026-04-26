22f23f2ecb9c99b5b2e052e4342ce503e41b8764
vyou <vyou@umich.edu>
2026-04-25 21:31:19 -0400

    fixed iframe

--- pages/index.js
+++ pages/index.js
@@ -499,7 +499,7 @@ const Home = () => (
         .
         <br/>
         <br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/HPjwlZ6knHg?si=rLtcegIKdSDnPPVC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/HPjwlZ6knHg?si=rLtcegIKdSDnPPVC" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
         <br/>
         Watch a pixel animation I made about my MMath research on using interactive visualizations to explain machine learning adversarial attacks.
         <br/>
